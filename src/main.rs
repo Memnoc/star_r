f67a1d55973ef248aa0ab4f9e8093ad62fcd0329
Matteo Stara <smartdroidesign@gmail.com>
2025-04-11 16:38:47 +0100

    parser for function calls

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use parse::parse_string;
+use parse::parse_call;
 
 mod parse;
 
 #[allow(unused_variables)]
 fn main() {
     let input = include_str!("../input.star");
-    let string = "\"Hello, world!\"";
-    parse_string(string);
+    let string = "println(\"hello, world!\")";
+    let _ = parse_call(string);
 }
--- src/parse.rs
+++ src/parse.rs
@@ -1,10 +1,12 @@
 use nom::{
     IResult, Parser,
     bytes::complete::{tag, take_until},
+    character::streaming::alpha1,
     sequence::delimited,
 };
 
 #[allow(dead_code)]
+#[derive(Debug)]
 pub enum Atom {
     String(String),
 }
@@ -18,3 +20,17 @@ pub fn parse_string(input: &str) -> IResult<&str, Atom> {
         .parse(input)
 }
 
+#[allow(dead_code)]
+#[derive(Debug)]
+pub enum Expr {
+    Call(String, Atom),
+}
+
+pub fn parse_call(input: &str) -> IResult<&str, Expr> {
+    let parse_name = alpha1;
+    let parse_arg = delimited(tag("("), parse_string, tag(")"));
+    let parser = (parse_name, parse_arg);
+    parser
+        .map(|(name, arg)| Expr::Call(name.to_string(), arg))
+        .parse(input)
+}
