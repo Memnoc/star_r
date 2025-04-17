eebe5b54793ea90dea365e8268a925d2b5d79a07
Memnoc <smartdroidesign@gmail.com>
2025-04-17 09:31:03 +0100

    added evaluation for the Atom

--- src/eval.rs
+++ src/eval.rs
@@ -1,12 +1,15 @@
 use crate::parse::*;
+use std::collections::HashMap;
 
-pub fn eval(expr: Expr) {
+pub fn eval(expr: Expr, context: &mut HashMap<String, Atom>) {
     match expr {
         Expr::Call(name, arg) => {
             if name == "println!" {
                 println!("{arg}");
             }
         }
-        _ => {}
+        Expr::Let(name, value) => {
+            context.insert(name, value);
+        }
     }
 }
